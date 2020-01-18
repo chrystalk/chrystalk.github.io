04e55f97de98fc484b3943ebf35fe6d94e52ca12
chrystalk <59623054+chrystalk@users.noreply.github.com>
2020-01-18 03:02:38 -0500

    rough draft

--- js/index.js
+++ js/index.js
@@ -11,3 +11,16 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
   document.body.style.backgroundColor = "white";
 }
+
+/* Link Pages in Menu
+function linkPage(){
+	var home= document.getElementById("Home")
+	home.href= "./html/index.html"
+	var music= document.getElementById("Music")
+	music.href= "./html/my-music.html"
+	var moodboards= document.getElementById("Moodboards")
+	moodboards.href= "./html/moodboards.html"
+	var contact= document.getElementById("Contact")
+	contact.href= "./html/contact.html"
+}
+*/
